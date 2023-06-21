997fd2a302996b10a08148d375fe639c8c43aa94
kelver-web <kelverwt@gmail.com>
2023-06-21 12:58:10 -0300

    modified url api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiCodeBurger = axios.create({
-  baseURL: 'http://localhost:3002'
+  baseURL: 'https://backend-codeburger-production.up.railway.app/'
 })
 
 apiCodeBurger.interceptors.request.use(async config => {
